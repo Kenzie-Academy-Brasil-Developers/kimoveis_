c0c2865e1ea51bb6e621776330774d72906e9d55
dnapolitanno <dudadnapolitano@gmail.com>
2023-05-09 15:42:04 -0300

    feat: create users route and middlewares to data

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
-import "reflect-metadata"
-import "express-async-errors"
-import express from "express"
+import "reflect-metadata";
+import "express-async-errors";
+import express from "express";
+import { handleErros } from "./error";
+import userRoutes from "./routes/users.routes";
 
+const app = express();
+app.use(express.json());
 
-const app = express()
-app.use(express.json())
+app.use("/users", userRoutes);
 
-
-export default app
+app.use(handleErros);
+export default app;
