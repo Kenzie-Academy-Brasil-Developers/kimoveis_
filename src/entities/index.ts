bad69120ebb377fa2893bdd7f42c3de344e7ec09
dnapolitanno <dudadnapolitano@gmail.com>
2023-05-09 11:39:05 -0300

    feat: create module export for entities

---
+++ src/entities/index.ts
@@ -0,0 +1,7 @@
+import { Address } from "./addresses.entities";
+import { Category } from "./categories.entities";
+import { RealEstate } from "./realEstate.entities";
+import { Schedule } from "./schedules.entities";
+import { User } from "./users.entities";
+
+export { User, Schedule, RealEstate, Address, Category };
